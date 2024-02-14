9ba161e98470fe6202cbd6e6f2ad5ea6c68effd5
Precious Longe <preciuzlp943@gmail.com>
2024-02-14 14:34:25 +0100

    updated the contact page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,20 +1,25 @@
-import "./ContactForm.css"
+import "./ContactForm.css";
 const ContactForm = () => {
   return (
     <div className="form">
-        <form>
-            <label htmlFor="name">Your Nam</label>
-            <input type="text" id="name" name="name" />
-            <label htmlFor="email">Email Address</label>
-            <input type="email" id="email" name="email" />
-            <label htmlFor="sub">Subject</label>
-            <input type="text" id="sub" name="subject" />
-            <label htmlFor="message">Message</label>
-            <textarea name="" id="message" rows="6" placeholder="Please describe the service you want me to provide for you" />
-            <button className="btn">Submit</button>
-        </form>
+      <form>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="email">Email Address</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="sub">Subject</label>
+        <input type="text" id="sub" name="subject" />
+        <label htmlFor="message">Message</label>
+        <textarea
+          name=""
+          id="message"
+          rows="6"
+          placeholder="Please describe the service you want me to provide for you"
+        />
+        <button className="btn">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
