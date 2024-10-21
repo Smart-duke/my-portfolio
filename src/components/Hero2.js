de02b2db6579edf87655c3412a1a73d4fbaabbec
Precious Longe <preciuzlp943@gmail.com>
2024-10-21 13:12:38 +0100

    favicon

--- src/components/Hero2.js
+++ src/components/Hero2.js
@@ -1,13 +1,15 @@
 import "./Hero2.css";
-const Hero2 = ({heading, text}) => {
-  return <div className="hero">
-    <div className="hero-img">
+const Hero2 = ({ heading, text }) => {
+  return (
+    <div className="hero">
+      <div className="hero-img">
         <div className="heading">
-            <h1>{heading}</h1>
-            <p>{text}</p>
+          <h1>{heading}</h1>
+          <p>{text}</p>
         </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Hero2;
